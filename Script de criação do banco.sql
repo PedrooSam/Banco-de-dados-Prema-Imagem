5046b5cfbfc7ac5d34479f51a0f9bcabece6023c
PedrooSam <pssa@cesar.school>
2025-04-08 11:49:05 -0300

    Finalização do script de criação do banco
    Script de criação finalizado, ainda falta corrigir os modelos para algumas alterações necessárias

--- Script de criação do banco.sql
+++ Script de criação do banco.sql
@@ -1,7 +1,7 @@
-create database PremaImagem
+create database PremaImagem;
 
 create table Colaborador(
-	id varchar primary key,
+	id varchar (12) primary key,
 	nome varchar(45),
 	cpf varchar(11)
 );
@@ -11,32 +11,32 @@ create table Empregado(
 	funcao varchar(10),
 	numeroPis int,
 	salario double,
-	empregado_id int,
-	foreign key(empregado_id) references Colaborador(id)
+	id varchar (12) PRIMARY KEY,
+	foreign key(id) references Colaborador(id)
 );
 
 create table Medico(
 	especialidade varchar(10),
 	crm varchar(45),
-	medico_id int,
-	foreign key(medico_id) references Colaborador(id)
+	id varchar (12) PRIMARY KEY,
+	foreign key(id) references Colaborador(id)
 );
 
 create table Socio(
-	proLabore int,
-	socio_idColaborador int,
-	foreign key(socio_idColaborador) references colaboradores(idColaborador)
+	proLabore double,
+	id varchar (12) PRIMARY KEY,
+	foreign key(id) references Colaborador(id)
 );
 
 CREATE TABLE Exame(
-	id varchar PRIMARY KEY,
+	id varchar (12) PRIMARY KEY,
 	nome varchar(50),
 	preparo varchar(100),
 	preco double
 );
 
 CREATE TABLE Paciente(
-	id varchar PRIMARY KEY,
+	id varchar (12) PRIMARY KEY,
 	nome varchar(45),
 	cpf varchar(11),
 	rg varchar(8),
@@ -45,8 +45,58 @@ CREATE TABLE Paciente(
 	complementoEndereco varchar(15),
 	telefone1 varchar(11),
 	telefone2 varchar(11),
-	email varchar(45),
+	email varchar(50),
 	nomeMae varchar(45),
-	pacienteIndicador varchar
+	pacienteIndicador varchar (12),
+	FOREIGN KEY (pacienteIndicador) REFERENCES Paciente(id)
 );
 
+CREATE TABLE AgendaExame(
+	id varchar (12) PRIMARY KEY,
+	dataRealizacao date,
+	medicoRequisitante varchar(45),
+	laudo varchar(1000),
+	status varchar(10),
+	idPaciente varchar(12),
+	idMedico  varchar(12),
+	idExame varchar(12),
+	FOREIGN key(idPaciente) REFERENCES Paciente(id),
+	FOREIGN KEY(idMedico) REFERENCES Medico(id),
+	FOREIGN KEY(idExame) REFERENCES Exame(id)
+);
+
+CREATE TABLE Pagamento(
+	formaPagamento varchar(21),
+	notaFiscal varchar(30),
+	valorPago double,
+	parcelas int,
+	id varchar(12),
+	idAgenda varchar(12),
+	FOREIGN KEY (idAgenda) REFERENCES Agenda(id)
+);
+
+CREATE TABLE Fornecedor(
+	id varchar(12),
+	nome varchar(45),
+	cnpj varchar(14),
+	telefone1 varchar(11),
+	telefone2 varchar(11),
+	email varchar(50)
+);
+
+CREATE TABLE Produto(
+	nome varchar (45),
+	preco double,
+	quantidade int,
+	id varchar (12)
+);
+
+CREATE TABLE Venda(
+	data_venda date,
+	idFornecedor varchar (12),
+	idProduto varchar (12),
+	idSocio varchar (12),
+	FOREIGN KEY (idFornecedor) REFERENCES Agenda(id),
+	FOREIGN KEY (idProduto) REFERENCES Agenda(id),
+	FOREIGN KEY (idSocio) REFERENCES Agenda(id)
+);
