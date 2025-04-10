e0b3ec783279d4f2a75032eac213bd55977f9c11
Felipe Matias <74963264+Zibec@users.noreply.github.com>
2025-04-10 08:11:16 -0300

    Base do spring

---
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/ProjetoBdApplication.java
@@ -0,0 +1,13 @@
+package com.premaImagem.projeto_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBdApplication.class, args);
+	}
+
+}
---
+++ projeto-bd/src/test/java/com/premaImagem/projeto_bd/ProjetoBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premaImagem.projeto_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
