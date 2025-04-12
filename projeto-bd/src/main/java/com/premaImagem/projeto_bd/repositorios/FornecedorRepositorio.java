bf6ab96100d38f0e4aed61ba25303bf9ec1db90d
Felipe Matias <74963264+Zibec@users.noreply.github.com>
2025-04-11 21:42:57 -0300

    CRUD Fornecedores

---
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/FornecedorRepositorio.java
@@ -0,0 +1,48 @@
+package com.premaImagem.projeto_bd.repositorios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.premaImagem.projeto_bd.entidades.Fornecedor;
+
+@Repository
+public class FornecedorRepositorio {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public FornecedorRepositorio(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Fornecedor> buscarLista() {
+        String sql = "SELECT * FROM Fornecedor";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Fornecedor.class));
+    }
+
+    public Fornecedor buscar(String id) {
+        String sql = "SELECT * FROM Fornecedor WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Fornecedor.class), id);
+    }
+
+    public int criar(Fornecedor fornecedor) {
+        String sql = "INSERT INTO Fornecedor (id, nome, cnpj, telefone1, telefone2, email) VALUES (?, ?, ?, ?, ?, ?)";
+        return jdbcTemplate.update(sql, fornecedor.getId(), fornecedor.getNome(), fornecedor.getCnpj(),
+                fornecedor.getTelefone1(), fornecedor.getTelefone2(), fornecedor.getEmail());
+    }
+
+    public int editar(Fornecedor fornecedor) {
+        String sql = "UPDATE Fornecedor SET nome = ?, cnpj = ?, telefone1 = ?, telefone2 = ?, email = ? WHERE id = ?";
+        return jdbcTemplate.update(sql, fornecedor.getNome(), fornecedor.getCnpj(), fornecedor.getTelefone1(),
+                fornecedor.getTelefone2(), fornecedor.getEmail(), fornecedor.getId());
+    }
+
+    public int deletar(String id) {
+        String sql = "DELETE FROM Fornecedor WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+}
