45058bfaf13a2518ada80596447545d6138add02
PedrooSam <pssa@cesar.school>
2025-04-17 20:13:21 -0300

    Adição do CRUD de Produtos
    CRUD funcional para produtos.

---
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/entidades/Produto.java
@@ -0,0 +1,46 @@
+package com.premaImagem.projeto_bd.entidades;
+
+public class Produto {
+    private long id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(long id, String nome, double preco, int quantidade){
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public long getId() {
+        return id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
---
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/ProdutoRepositorio.java
@@ -0,0 +1,51 @@
+package com.premaImagem.projeto_bd.repositorios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.premaImagem.projeto_bd.entidades.Produto;
+
+@Repository
+public class ProdutoRepositorio {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProdutoRepositorio(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Produto> buscarLista(){
+        String sql = "SELECT * FROM Produto";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Produto.class));
+    }
+
+    public Produto buscarPorNome(String nome){
+        String sql = "SELECT * FROM Produto WHERE Produto.nome = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Produto.class), nome);
+    }
+
+    public Produto buscarPorId(long id){
+        String sql = "SELECT * FROM Produto WHERE Produto.id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Produto.class), id);
+    }
+
+    public int criar(Produto produto){
+        String sql = "INSERT INTO Produto (id, nome, preco) VALUES (?, ?, ?)";
+        return jdbcTemplate.update(sql, produto.getId(), produto.getNome(), produto.getPreco());
+    }
+
+    public int editar(Produto produto){
+        String sql = "UPDATE Produto SET nome = ?, preco = ? WHERE Produto.id = ?";
+        return jdbcTemplate.update(sql, produto.getNome(), produto.getPreco(), produto.getId());
+    }
+
+    public int deletar(long id){
+        String sql = "DELETE FROM Produto WHERE Produto.id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+}
