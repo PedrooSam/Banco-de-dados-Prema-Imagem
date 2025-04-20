a585c7b4b99941483523c57a5ee806a7d4dc8339
PedrooSam <pssa@cesar.school>
2025-04-20 17:10:44 -0300

    EmpregadoRepositorio feito

--- projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/EmpregadoRepositorio.java
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/EmpregadoRepositorio.java
@@ -6,44 +6,58 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import com.premaImagem.projeto_bd.entidades.Exames;
+import com.premaImagem.projeto_bd.entidades.Empregado;
 
-//TODO FUDIDO ESSE AQUI
 
 @Repository
-public class ExameRepositorio{
+public class EmpregadoRepositorio{
 
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public ExameRepositorio(JdbcTemplate jdbcTemplate) {
+    public EmpregadoRepositorio(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Exames> buscarLista(){
-        String sql = "SELECT * FROM Exame";
-        List<Exames> exames = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Exames.class));
+    public List<Empregado> buscarLista(){
+        String sql = "SELECT * FROM Empregado INNER JOIN Colaborador ON Empregado.id = Colaborador.id";
+        List<Empregado> exames = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Empregado.class));
         return exames;
     }
 
-    public Exames buscar(long nomeExame){
-        String sql = "SELECT * FROM Exame WHERE Exame.nome = ?";
-        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Exames.class), nomeExame);
+    public Empregado buscarPorNome(String nome){
+        String sql = "SELECT * FROM Empregado INNER JOIN Colaborador ON Empregado.id = Colaborador.id WHERE Colaborador.nome = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Empregado.class), nome);
     }
 
-    public int criar(Exames exame){
-        String sql = "INSERT INTO Exame (nome, preparo, preco) VALUES (?,?,?)";
-        return jdbcTemplate.update(sql, exame.getNome(), exame.getPreparo(), exame.getPreco());
+    public Empregado buscarPorId(long id){
+        String sql = "SELECT * FROM Empregado INNER JOIN Colaborador ON Empregado.id = Colaborador.id WHERE Colaborador.id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Empregado.class), id);
     }
 
-    public int editar(Exames exame){
-        String sql = "UPDATE Exame SET Exame.nome = ?, Exame.preparo = ?, Exame.preco = ? WHERE id = ?";
-        return jdbcTemplate.update(sql, exame.getNome(), exame.getPreparo(), exame.getPreco(), exame.getId());
+    public int criar(Empregado empregado){
+
+        String sqlColaborador = "INSERT INTO Colaborador (cpf, nome) VALUES (?, ?)";
+        jdbcTemplate.update(sqlColaborador, empregado.getCpf(), empregado.getNome());
+
+        String sqlEmpregado = "INSERT INTO Empregado (id, numeroPis, salario, funcao, dataAdmissao) VALUES (?, ?, ?, ?, ?)";
+        return jdbcTemplate.update(sqlEmpregado, empregado.getId(), empregado.getNumeroPis(), empregado.getSalario(), empregado.getFuncao(), empregado.getDataAdmissao());
+    }
+
+    public int editar(Empregado empregado){
+        String sqlColaborador = "UPDATE Colaborador SET cpf = ?, nome = ? WHERE id = ?";
+        jdbcTemplate.update(sqlColaborador, empregado.getCpf(), empregado.getNome(), empregado.getId());
+
+        String sqlEmpregado = "UPDATE Empregado SET numeroPis = ?, salario = ?, funcao = ?, dataAdmissao = ? WHERE id = ?";
+        return jdbcTemplate.update(sqlEmpregado, empregado.getNumeroPis(), empregado.getSalario(), empregado.getFuncao(), empregado.getDataAdmissao(), empregado.getId());
     }
 
     public int deletar(long id){
-        String sql = "DELETE FROM Exame WHERE id = ?";
-        return jdbcTemplate.update(sql, id);
+        String sqlColaborador = "DELETE FROM Colaborador WHERE id = ?";
+        jdbcTemplate.update(sqlColaborador, id);
+
+        String sqlEmpregado = "DELETE FROM Empregado WHERE id = ?";
+        return jdbcTemplate.update(sqlEmpregado, id);
     }
 
 }
