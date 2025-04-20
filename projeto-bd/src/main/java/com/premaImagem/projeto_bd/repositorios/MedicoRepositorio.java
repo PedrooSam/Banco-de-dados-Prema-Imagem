4fc1c4adf068a95d2fafedab005411630a3d9906
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-04-20 16:05:53 -0300

    repositorio de medico feito
    fiz os buscar e esse vai ser o modelo padrao para as herenças

--- projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/MedicoRepositorio.java
+++ projeto-bd/src/main/java/com/premaImagem/projeto_bd/repositorios/MedicoRepositorio.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import com.premaImagem.projeto_bd.entidades.Medico;
+import org.springframework.transaction.annotation.Transactional;
 
 //CORRIGIR PRA HERANÇA NOVA (NÃO SEI COMO FAZER AINDA)
 
@@ -21,29 +22,54 @@ public class MedicoRepositorio{
     }
 
     public List<Medico> buscarLista(){
-        String sql = "SELECT * FROM Medico";
-        List<Medico> medicos = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Medico.class));
-        return medicos;
+        String sql = "SELECT c.id, c.nome, c.cpf, m.crm, m.especialidade FROM Colaborador c INNER JOIN Medico m ON c.id = m.id";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Medico.class), id);
     }
 
-    public Colaborador buscar(long nome){
-        String sql = "SELECT * FROM Colaborador WHERE Colaborador.nome = ?";
-        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Colaborador.class), nome);
+    public Medico buscarNome(String nome){
+        String sqlMedico = "SELECT c.id, c.nome, c.cpf, m.crm, m.especialidade FROM Colaborador c INNER JOIN Medico m ON c.id = m.id WHERE c.nome = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Medico.class), nome);
     }
 
-    public int criar(Colaborador colaborador, Medicos medico){
-        String sql = "INSERT INTO Colaborador (id, cpf, nome) VALUES (?,?,?), Medico (crm, especialidade) VALUES (?,?)";
-        return jdbcTemplate.update(sql, colaborador.getId(), colaborador.getCpf(), colaborador.getNome(), medico.getCrm(), medico.getEspecialidade());
+    //buscar por id
+    public Medico buscarId(long id){
+        String sqlMedico = "SELECT c.id, c.nome, c.cpf, m.crm, m.especialidade FROM Colaborador c INNER JOIN Medico m ON c.id = m.id WHERE c.id = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Medico.class), id);
     }
 
-    public int editar(Colaborador colaborador, Medico medico){
-        String sql = "UPDATE Colaborador SET Colaborador.nome = ?, Medico.especialidade = ? WHERE id = ?";
-        return jdbcTemplate.update(sql, colaborador.getNome(), Medico.Especialidade(), colaborador.getId());
+    //buscar por crm
+    public Medico buscarNome(String crm){
+        String sqlMedico = "SELECT c.id, c.nome, c.cpf, m.crm, m.especialidade FROM Colaborador c INNER JOIN Medico m ON c.id = m.id WHERE m.crm = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Medico.class), crm);
+    }
+    
+    @Transactional
+    public int criar(Medico medico){
+        String sqlColaborador = "INSERT INTO Colaborador (cpf, nome) VALUES (?, ?)";
+        jdbcTemplate.update(sqlColaborador, medico.getCpf(), medico.getNome());
+
+        long id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+
+        String sqlMedico = "INSERT INTO Medico (id, crm, especialidade) VALUES (?, ?, ?)";
+        return jdbcTemplate.update(sqlMedico, medico.getId(),medico.getCrm(), medico.getEspecialidade());
     }
 
+    @Transactional
+    public int editar(Medico medico){
+        String sqlColaborador = "UPDATE Colaborador SET Colaborador.nome = ?,  cpf = ? WHERE id = ?";
+        jdbcTemplate.update(sqlColaborador, medico.getNome(), Medico.getCpf(), medico.getId());
+
+        String sql Medico = "UPDATE Medico SET Medico.crm = ?, Medico.especialidade = ? Where Medico.id = ?";
+        return jdbcTemplate.update(sqlMedico, medico.getCrm(), medico.getEspecialidade, medico.getId());
+    }
+
+    @Transactional
     public int deletar(long id){
-        String sql = "DELETE FROM Colaborador WHERE id = ?";
-        return jdbcTemplate.update(sql, id);
+        String sqlMedico = "DELETE FROM Medico WHERE id = ?";
+        jdbcTemplate.update(sqlMedico, id);
+
+        String sqlColaborador = "DELETE FROM Colaborador Where id = ?";
+        return jdbcTemplate.update(sqlColaborador, id);
     }
 
 }
